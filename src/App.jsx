c211b384fe13ae6a1ecf833bddbc98f64ecaeb8e
Alejandro Vasquez <josejaguer12@gmail.com>
2024-06-11 22:10:12 -0600

    Ya tenemos el delete y el cambio de state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { Title } from "./components/Title";
 import { InputTodo } from "./components/InputTodo";
-import { TodoList } from "./components/TodoList";
 import { TodoItem } from "./components/TodoItem";
 
 function App() {
   const [todo, setTodo] = useState([
     { task: "Programar", state: false },
     { task: "Leer un libro", state: true },
+    { task: "Hacer algo", state: true },
+    { task: "Bailar", state: true },
+    { task: "Cocinar", state: false },
+    { task: "Laugh", state: false },
+    { task: "Estudiar", state: false },
   ]);
 
   const totalTodos = todo.length;
@@ -19,11 +23,39 @@ function App() {
     setTodo([{ task: newTask, state: false }, ...todo]);
   };
 
+  const handleState = (task) => {
+    const newState = todo.map((todo) => {
+      if (todo.task === task) {
+        return { task: todo.task, state: !todo.state };
+      } else {
+        return todo;
+      }
+    });
+
+    setTodo(newState);
+  };
+
+  const deleteTask = (task) => {
+    setTodo(todo.filter((todo) => todo.task !== task));
+  };
+
   return (
     <div className="container">
       <Title total={totalTodos} completed={completedTodos} />
       <InputTodo addNewTask={addNewTask} />
-      <TodoList todos={todo} />
+      <ul>
+        {todo.map((todo, i) => {
+          return (
+            <TodoItem
+              key={i}
+              deleteTask={deleteTask}
+              handleState={handleState}
+              task={todo.task}
+              state={todo.state}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,26 @@
-function TodoItem() {
-  return <li>Hola</li>;
+import { RiCheckboxBlankLine } from "@remixicon/react";
+import { RiCheckboxFill } from "@remixicon/react";
+
+function TodoItem({ task, state, deleteTask, handleState }) {
+  return (
+    <li>
+      <div>
+        <i onClick={() => handleState(task)} className="icon">
+          {state ? (
+            <RiCheckboxFill size={40} color="#008000" />
+          ) : (
+            <RiCheckboxBlankLine size={40} />
+          )}
+        </i>
+        <div>
+          <p>{task}</p>
+          <button className="btn-delete" onClick={() => deleteTask(task)}>
+            Eliminar
+          </button>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export { TodoItem };
